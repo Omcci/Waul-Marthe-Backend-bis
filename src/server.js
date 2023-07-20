0cda9ae2be5622b1df3e249307a3080282c5c967
Omci <melloulchi.omar@gmail.com>
2023-07-20 22:28:58 +0200

    added routes on backend

--- src/server.js
+++ src/server.js
@@ -1,22 +1,39 @@
-import Hapi from '@hapi/hapi'
-import routes from './routes'
-import { db } from './database'
+import Hapi from "@hapi/hapi";
+import routes from "./routes";
+import { db } from "./database";
 
 const start = async () => {
-    const server = Hapi.server({
-        port: 8010,
-        host: "localhost",
-    })
+  const server = Hapi.server({
+    port: 8010,
+    host: "localhost",
+  });
 
-    routes.forEach(route => server.route(route))
+  routes.forEach((route) => server.route(route));
+
+  try {
+    console.log("Starting the server...");
 
     await server.start();
-    console.log(`Server is listening on ${server.info.uri}`)
-}
+    console.log(`Server is listening on ${server.info.uri}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
-process.on('unhandledRejection', err => {
-    console.log(err)
-    process.exit(1)
-})
+// process.on('unhandledRejection', err => {
 
-start()
+// })
+
+process.on("SIGINT", async () => {
+  console.log("Stopping server...");
+
+  await server.stop({ timeout: 10000 });
+
+  db.end();
+
+  console.log("Server stopped");
+  process.exit(0);
+});
+
+start();
